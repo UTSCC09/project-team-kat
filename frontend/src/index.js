8af4afccab08545deca07da1f6287ae3639de15c
Ammar <ammart500@gmail.com>
2022-03-22 17:04:54 -0400

    Remove extra comment

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -23,11 +23,6 @@ const wsLink = new GraphQLWsLink(createClient({
   url: 'ws://localhost:3000/subscriptions',
 }));
 
-// The split function takes three parameters:
-//
-// * A function that's called for each operation to execute
-// * The Link to use for an operation if the function returns a "truthy" value
-// * The Link to use for an operation if the function returns a "falsy" value
 const splitLink = split(
     ({query}) => {
       const definition = getMainDefinition(query);
